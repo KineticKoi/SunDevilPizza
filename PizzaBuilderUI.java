85e631d7be07c86f62e8e8ffcf8f2ca3b44cb16b
KineticKoi <64819201+KineticKoi@users.noreply.github.com>
2022-11-03 15:43:42 -0700

    SunDevilPizzaApp

--- PizzaBuilderUI.java
+++ PizzaBuilderUI.java
@@ -1,5 +1,7 @@
+import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
@@ -12,13 +14,22 @@ public class PizzaBuilderUI extends Pane {
     private final String[] sizeButtonText = new String[] {"Small", "Medium", "Large"};
     private final String[] typeButtonText = new String[] {"Cheese", "Pepperoni", "Vegetable"};
     private final String[] toppingButtonText = new String[] {"Lt", "Reg", "Ex"};
-    private final String[] hoursButtonText = new String[] {"12:00PM", "1:00PM", "2:00PM", "3:00PM", "4:00PM", "5:00PM", "6:00PM", "7:00PM", "8:00PM", "9:00PM"};
+    private String[] timeOptions = new String[] {"12:00PM", "12:15PM", "12:30PM", "12:45PM", 
+                                                    "1:00PM", "1:15PM", "1:30PM", "1:45PM", 
+                                                    "2:00PM", "2:15PM", "2:30PM", "2:45PM", 
+                                                    "3:00PM", "3:15PM", "3:30PM", "3:45PM", 
+                                                    "4:00PM", "4:15PM", "4:30PM", "4:45PM", 
+                                                    "5:00PM", "5:15PM", "5:30PM", "5:45PM", 
+                                                    "6:00PM", "6:15PM", "6:30PM", "6:45PM",
+                                                    "7:00PM", "7:15PM", "7:30PM", "7:45PM",
+                                                    "8:00PM", "8:15PM", "8:30PM", "8:45PM",
+                                                    "9:00PM", "9:15PM", "9:30PM", "9:45PM"};
     private Button backButton;
     private Button forwardButton;
     private Label customizationLabel;
     private Label pizzaToppingsLabel;
     private Label timeSelectionLabel;
-    private SelectionBar timePicker;
+    private ChoiceBox<String> timePicker;
     private ScrollPane toppingsSP;
     private LargeSelectionBar sizeSelector;
     private LargeSelectionBar pizzaBaseSelector;
@@ -42,8 +53,20 @@ public class PizzaBuilderUI extends Pane {
         timeSelectionLabel = new Label("Pickup Time:");
         timeSelectionLabel.relocate(100, 830);
         timeSelectionLabel.setFont(new Font("Arial", 40));
-        timePicker = new SelectionBar("time", "", 10, hoursButtonText);
-        timePicker.relocate(-200, 900);
+        timePicker = new ChoiceBox<>();
+        timePicker.getItems().addAll("12:00PM", "12:15PM", "12:30PM", "12:45PM", 
+                                    "1:00PM", "1:15PM", "1:30PM", "1:45PM", 
+                                    "2:00PM", "2:15PM", "2:30PM", "2:45PM", 
+                                    "3:00PM", "3:15PM", "3:30PM", "3:45PM", 
+                                    "4:00PM", "4:15PM", "4:30PM", "4:45PM", 
+                                    "5:00PM", "5:15PM", "5:30PM", "5:45PM", 
+                                    "6:00PM", "6:15PM", "6:30PM", "6:45PM",
+                                    "7:00PM", "7:15PM", "7:30PM", "7:45PM",
+                                    "8:00PM", "8:15PM", "8:30PM", "8:45PM",
+                                    "9:00PM", "9:15PM", "9:30PM", "9:45PM");
+        timePicker.setMinSize(240, 40);
+        timePicker.relocate(100, 900);
+        timePicker.setOnAction(new PizzaBuilderControlsHandler());
         toppingsSP = new ScrollPane();
         toppingsSP.relocate(100, 600);
         toppingsSP.setPrefWidth(600);
@@ -99,7 +122,7 @@ public class PizzaBuilderUI extends Pane {
             if (event.getSource() == backButton) {
                 SunDevilPizza.previousRoot();
             }
-            if (event.getSource() == forwardButton) {     
+            else if (event.getSource() == forwardButton) {     
                 Pizza currentPizza = ((Customer)SunDevilPizza.session.getUser()).getCurrentOrder().getPizza();
                 if (currentPizza.getSize() != null && currentPizza.getType() != null) {
                     if (SunDevilPizza.nextRoot() == false) {
@@ -109,6 +132,10 @@ public class PizzaBuilderUI extends Pane {
                 else {
                     customizationLabel.setStyle("-fx-text-fill: red;");
                 }
+            }
+            else if (event.getSource() == timePicker) {
+                ((Customer)SunDevilPizza.session.getUser()).getCurrentOrder().setPickupTime(timePicker.getValue());
+                refreshOrderSummary();
             }
         }
     }
