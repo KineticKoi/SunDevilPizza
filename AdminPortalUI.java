0b32af9434f76a0987afdb887c1372d811091795
CamJam2048 <115676804+CamJam2048@users.noreply.github.com>
2022-11-03 22:39:32 -0700

    Update AdminPortalUI.java
    added comments

--- AdminPortalUI.java
+++ AdminPortalUI.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 
+//
 public class AdminPortalUI extends Pane{
     //Declaring Variables...
     private Label headerLabel;
@@ -14,22 +15,23 @@ public class AdminPortalUI extends Pane{
     AdminPortalUI(int width, int height) {
         setWidth(width); //Sets this pane width
         setHeight(height); //Sets this pane height
-        setStyle("-fx-background-color: #FFFFFF");
-        headerLabel = new Label("ADMIN PORTAL (UNDER CONSTRUCTION):");
-        headerLabel.setFont(new Font("Arial", 40));
-        headerLabel.layoutXProperty().set(40);
-        headerLabel.layoutYProperty().set(40);
-        homeButton = new ButtonMaker("home");
-        homeButton.setOnAction(new AdminPortalControlsHandler());
-        getChildren().addAll(headerLabel, homeButton);
+        setStyle("-fx-background-color: #FFFFFF"); //Sets pane background to white
+        headerLabel = new Label("ADMIN PORTAL (UNDER CONSTRUCTION):"); //Sets pane label text
+        headerLabel.setFont(new Font("Arial", 40)); //Sets label text font type and size
+        headerLabel.layoutXProperty().set(40); //Sets label X value
+        headerLabel.layoutYProperty().set(40); //Sets label Y value
+        homeButton = new ButtonMaker("home"); //Sets button text
+        homeButton.setOnAction(new AdminPortalControlsHandler()); //Call to event handler
+        getChildren().addAll(headerLabel, homeButton); //Adding home button and label to pane
     }
     
+    //Event handler
     private class AdminPortalControlsHandler implements EventHandler<javafx.event.ActionEvent> {
         @Override
         public void handle(javafx.event.ActionEvent event) {
-            Sounds.playButtonClick();
+            Sounds.playButtonClick(); //Adds a sound when pane is clicked
             if (event.getSource() == homeButton) {
-                SunDevilPizza.home();
+                SunDevilPizza.home(); //Takes user to home page if button is clicked
             }
         }
     }
