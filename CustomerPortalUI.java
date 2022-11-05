3d8d958ebec4a6688a80a690330c200cae68749c
CamJam2048 <115676804+CamJam2048@users.noreply.github.com>
2022-11-05 14:51:21 -0700

    Update CustomerPortalUI.java

--- CustomerPortalUI.java
+++ CustomerPortalUI.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 
+//Class builds customer portal UI pane
 public class CustomerPortalUI extends Pane{
     //Declaring Variables...
     private Label headerLabel;
@@ -14,23 +15,26 @@ public class CustomerPortalUI extends Pane{
     CustomerPortalUI(int width, int height) {
         setWidth(width); //Sets this pane width
         setHeight(height); //Sets this pane height
-        setStyle("-fx-background-color: #FFFFFF");
-        headerLabel = new Label("CUSTOMER PORTAL (UNDER CONSTRUCTION):");
-        headerLabel.setFont(new Font("Arial", 40));
-        headerLabel.layoutXProperty().set(40);
-        headerLabel.layoutYProperty().set(40);
-        homeButton = new ButtonMaker("home");
-        homeButton.setOnAction(new CustomerPortalControlsHandler());
-        getChildren().addAll(headerLabel, homeButton);
+        setStyle("-fx-background-color: #FFFFFF"); //Sets background to be white
+        headerLabel = new Label("CUSTOMER PORTAL (UNDER CONSTRUCTION):"); //Sets pane label text
+        headerLabel.setFont(new Font("Arial", 40)); //Sets label text font and size
+        headerLabel.layoutXProperty().set(40); //Sets pane's X value
+        headerLabel.layoutYProperty().set(40); //Sets pane's Y value
+        homeButton = new ButtonMaker("home"); //Creates new button with "home" text 
+        homeButton.setOnAction(new CustomerPortalControlsHandler()); //Sets up pane's button handler
+        getChildren().addAll(headerLabel, homeButton); //Adds label and home button to pane
     }
     
+    //Event handler for pane's home button and audio for clicking
     private class CustomerPortalControlsHandler implements EventHandler<javafx.event.ActionEvent> {
         @Override
         public void handle(javafx.event.ActionEvent event) {
-            Sounds.playButtonClick();
-            if (event.getSource() == homeButton) {
-                SunDevilPizza.home();
+            Sounds.playButtonClick(); //Calls sound class to play audio when pane is clicked
+            if (event.getSource() == homeButton) { //Checking if home button is clicked
+                SunDevilPizza.home(); //Takes user back to main landing page when home button is clicked
             }
         }
     }
-}
+    
+} //End of CustomerPortalUI class
+
