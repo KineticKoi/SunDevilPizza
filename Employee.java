5a38cd45f3f7bbc26bdec9b6a6dc330c618b7adb
CamJam2048 <115676804+CamJam2048@users.noreply.github.com>
2022-11-05 14:54:46 -0700

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,15 +1,17 @@
 
-//Employee class...
+//Employee class with getter and setter methods for employee role
 public class Employee extends User {
-    private String role;
+    private String role; //role includes order processing agent and chef employees
     
     //Default Constructor
     Employee(String role) {
-        super("EMPLOYEE");
-        this.role = role;
+        super("EMPLOYEE"); //Super to pull employee object information 
+        this.role = role; //Updates employee object's role
     }
     
+    //Getter method for returning employee role 
     public String getRole() {
         return role;
     }
-}
+    
+} //End of Employee class
