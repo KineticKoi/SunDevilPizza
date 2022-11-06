4756f4eab71735cccc6cf1239f159bd24f594d07
CamJam2048 <115676804+CamJam2048@users.noreply.github.com>
2022-11-06 11:57:55 -0700

    Update LargeSelectionBar.java

--- LargeSelectionBar.java
+++ LargeSelectionBar.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 import java.util.List;
 import javafx.event.EventHandler;
@@ -7,7 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 
-//Class 
+//Class builds Large selection bars for use throughout program
 public class LargeSelectionBar extends Pane {
     //Declaring Variables...
     private Label optionLabel;
@@ -18,23 +17,23 @@ public class LargeSelectionBar extends Pane {
     
     //Constructor
     LargeSelectionBar(String selectionType, String labelText, int numOfButtons, String[] buttonText) {
-        setWidth(600); //Sets current pane's width to 600
-        setHeight(400); //Sets current pane's height to 400
+        setWidth(600); //Sets this pane width
+        setHeight(400); //Sets this pane height
         this.selectionType = selectionType;
         optionLabel = new Label(labelText); //Sets label text from labelText string in method parameters
         optionLabel.setFont(new Font("Arial", 28)); //Sets label text size and font
         optionLabel.relocate(5, 5); //Relocates label 
         buttonList = new ArrayList<>(numOfButtons); //Creates a new list ArrayList of buttons
         getChildren().add(optionLabel); //Adds created label to pane
-        for (int i = 0; i < numOfButtons; i++) { //Looping for number of buttons from numOfButtons integer in method parameters 
-            buttonList.add(new Button()); //Adding a new button to the button list
-            buttonList.get(i).setText(buttonText[i]); //Setting the text of each button from buttonText array in method parameters 
-            buttonList.get(i).setPrefSize(220, 80); //Setting preferred size of current button at buttonList index to (220, 80)
-            buttonList.get(i).relocate((i * 260), 60); //Relocating current button at buttonList index to be (index * 260), 60)
-            buttonList.get(i).setStyle(defaultButtonStyle); //Setting style of new button to have black text and a grey background
-            buttonList.get(i).setFont(new Font("Arial", 28)); //Setting button text size and font
-            buttonList.get(i).setOnAction(new ControlsHandler()); //Sets control handler for actions for the new button 
-            getChildren().add(buttonList.get(i)); //Adding current indexed button to pane 
+        for (int i = 0; i < numOfButtons; i++) { //Looping for number of buttons that selection bar will have
+            buttonList.add(new Button()); //Adds new button to the list
+            buttonList.get(i).setText(buttonText[i]); //Sets text of current indexed button
+            buttonList.get(i).setPrefSize(220, 80); //Set's current indexed button's preffered size to (220, 80)
+            buttonList.get(i).relocate((i * 260), 60); //Relocate button 
+            buttonList.get(i).setStyle(defaultButtonStyle); //Sets style of new button to have black text and lightgrey background
+            buttonList.get(i).setFont(new Font("Arial", 28)); //Set's button text font and size
+            buttonList.get(i).setOnAction(new ControlsHandler()); //Sets up control handler for button
+            getChildren().add(buttonList.get(i)); //Adding current button to list 
         }
     }
     
@@ -44,16 +43,16 @@ public class LargeSelectionBar extends Pane {
         public void handle(javafx.event.ActionEvent event) {
             Sounds.playButtonClick(); //Calls sound class to play audio when a button is clicked
             if (currentSelection == (Button)event.getSource()) { //Checks if button has already been toggled
-                currentSelection.setStyle(defaultButtonStyle); //Sets button color to the default light grey 
+                currentSelection.setStyle(defaultButtonStyle); //Sets button color to the default light grey
                 if (selectionType.equals("size")) { //Checking if button labeled "size" was clicked
                     ((Customer)SunDevilPizza.session.getUser()).getCurrentOrder().getPizza().setSize(null);
                     ((PizzaBuilderUI)currentSelection.getScene().getRoot()).refreshOrderSummary(); //Updates pizza being currently built to show changes
                 }
                 else if (selectionType.equals("type")) { //Checking if button labeled "type" was clicked
-                    ((Customer)SunDevilPizza.session.getUser()).getCurrentOrder().getPizza().setType(null); 
+                    ((Customer)SunDevilPizza.session.getUser()).getCurrentOrder().getPizza().setType(null);
                     ((PizzaBuilderUI)currentSelection.getScene().getRoot()).refreshOrderSummary(); //Updates pizza being currently built to show changes
                 }
-                currentSelection = null;
+                currentSelection = null; //Setting button clikced to null to work as a toggle 
             }
             else {
                 currentSelection = (Button)event.getSource(); //Checks if button has not been toggled
@@ -61,15 +60,14 @@ public class LargeSelectionBar extends Pane {
                     ((Customer)SunDevilPizza.session.getUser()).getCurrentOrder().getPizza().setSize(((Button)event.getSource()).getText());
                     ((PizzaBuilderUI)currentSelection.getScene().getRoot()).refreshOrderSummary(); //Updates pizza being currently built to show changes
                 }
-                else if (selectionType.equals("type")) { //Checking if button labeled "type" was clicked 
+                else if (selectionType.equals("type")) { //Checking if button labeled "type" was clicked
                     ((Customer)SunDevilPizza.session.getUser()).getCurrentOrder().getPizza().setType(((Button)event.getSource()).getText());
                     ((PizzaBuilderUI)currentSelection.getScene().getRoot()).refreshOrderSummary(); //Updates pizza being currently built to show changes
                 }
                 for (int i = 0; i < buttonList.size(); i++) { //Looping for size of button list
-                    buttonList.get(i).setStyle(defaultButtonStyle); //Setting style of each button in button list
+                    buttonList.get(i).setStyle(defaultButtonStyle); //Setting style of each button in button list to have black text and a lightgrey background
                 }
-                //Sets color of button text to black and background to light green to show that it has been toggled
-                currentSelection.setStyle("-fx-text-fill: black; -fx-background-color: #90ee90"); 
+                currentSelection.setStyle("-fx-text-fill: black; -fx-background-color: #90ee90"); //Toggling button to change background to light green
             }
         }
     }
