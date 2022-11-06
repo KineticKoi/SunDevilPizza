4a3c14215465060ea6473ca28d2abf09cece14f4
CamJam2048 <115676804+CamJam2048@users.noreply.github.com>
2022-11-05 18:06:05 -0700

    Update LargeSelectionBar.java
    Need to add more to ControlsHandler method

--- LargeSelectionBar.java
+++ LargeSelectionBar.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.List;
 import javafx.event.EventHandler;
@@ -6,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 
+//Class 
 public class LargeSelectionBar extends Pane {
     //Declaring Variables...
     private Label optionLabel;
@@ -18,21 +20,21 @@ public class LargeSelectionBar extends Pane {
     LargeSelectionBar(String selectionType, String labelText, int numOfButtons, String[] buttonText) {
         setWidth(600); //Sets this pane width
         setHeight(400); //Sets this pane height
-        this.selectionType = selectionType;
-        optionLabel = new Label(labelText);
-        optionLabel.setFont(new Font("Arial", 28));
-        optionLabel.relocate(5, 5);
-        buttonList = new ArrayList<>(numOfButtons);
-        getChildren().add(optionLabel);
-        for (int i = 0; i < numOfButtons; i++) {
-            buttonList.add(new Button());
-            buttonList.get(i).setText(buttonText[i]);
-            buttonList.get(i).setPrefSize(220, 80);
-            buttonList.get(i).relocate((i * 260), 60);
-            buttonList.get(i).setStyle(defaultButtonStyle);
-            buttonList.get(i).setFont(new Font("Arial", 28));
-            buttonList.get(i).setOnAction(new ControlsHandler());
-            getChildren().add(buttonList.get(i));
+        this.selectionType = selectionType; //
+        optionLabel = new Label(labelText); //Sets label text from labelText string in method parameters
+        optionLabel.setFont(new Font("Arial", 28)); //Sets label text size and font
+        optionLabel.relocate(5, 5); //Relocates label 
+        buttonList = new ArrayList<>(numOfButtons); //Creates a new list ArrayList of buttons
+        getChildren().add(optionLabel); //Adds created label to pane
+        for (int i = 0; i < numOfButtons; i++) { //Looping for number of buttons from numOfButtons integer in method parameters 
+            buttonList.add(new Button()); //Adding a new button to the button list
+            buttonList.get(i).setText(buttonText[i]); //Setting the text of each button from buttonText array in method parameters 
+            buttonList.get(i).setPrefSize(220, 80); //Setting preferred size of current button at buttonList index to (220, 80)
+            buttonList.get(i).relocate((i * 260), 60); //Relocating current button at buttonList index to be (index * 260), 60)
+            buttonList.get(i).setStyle(defaultButtonStyle); //Setting style of new button to have black text and a grey background
+            buttonList.get(i).setFont(new Font("Arial", 28)); //Setting button text size and font
+            buttonList.get(i).setOnAction(new ControlsHandler()); //Sets control handler for actions for the new button 
+            getChildren().add(buttonList.get(i)); //Adding current indexed button to pane 
         }
     }
     
@@ -40,14 +42,14 @@ public class LargeSelectionBar extends Pane {
     private class ControlsHandler implements EventHandler<javafx.event.ActionEvent> {
         @Override
         public void handle(javafx.event.ActionEvent event) {
-            Sounds.playButtonClick(); //Plays button click sound
+            Sounds.playButtonClick(); //Calls sound class to play audio when a button is clicked
             if (currentSelection == (Button)event.getSource()) {
-                currentSelection.setStyle(defaultButtonStyle);
-                if (selectionType.equals("size")) {
+                currentSelection.setStyle(defaultButtonStyle); //Sets button color default to be light grey when not clicked
+                if (selectionType.equals("size")) { //Checks if a button for pizza size is clicked
                     ((Customer)SunDevilPizza.session.getUser()).getCurrentOrder().getPizza().setSize(null);
                     ((PizzaBuilderUI)currentSelection.getScene().getRoot()).refreshOrderSummary();
                 }
-                else if (selectionType.equals("type")) {
+                else if (selectionType.equals("type")) { //Checks if a button for pizza type is clicked
                     ((Customer)SunDevilPizza.session.getUser()).getCurrentOrder().getPizza().setType(null);
                     ((PizzaBuilderUI)currentSelection.getScene().getRoot()).refreshOrderSummary();
                 }
@@ -55,19 +57,20 @@ public class LargeSelectionBar extends Pane {
             }
             else {
                 currentSelection = (Button)event.getSource();
-                if (selectionType.equals("size")) {
+                if (selectionType.equals("size")) { //Checking if a button clicked has text "size"
                     ((Customer)SunDevilPizza.session.getUser()).getCurrentOrder().getPizza().setSize(((Button)event.getSource()).getText());
                     ((PizzaBuilderUI)currentSelection.getScene().getRoot()).refreshOrderSummary();
                 }
-                else if (selectionType.equals("type")) {
+                else if (selectionType.equals("type")) { //Checking of a button 
                     ((Customer)SunDevilPizza.session.getUser()).getCurrentOrder().getPizza().setType(((Button)event.getSource()).getText());
                     ((PizzaBuilderUI)currentSelection.getScene().getRoot()).refreshOrderSummary();
                 }
-                for (int i = 0; i < buttonList.size(); i++) {
-                    buttonList.get(i).setStyle(defaultButtonStyle);
+                for (int i = 0; i < buttonList.size(); i++) { //Looping for size of button list
+                    buttonList.get(i).setStyle(defaultButtonStyle); //Setting style of each button in button list
                 }
-                currentSelection.setStyle("-fx-text-fill: black; -fx-background-color: #90ee90");
+                currentSelection.setStyle("-fx-text-fill: black; -fx-background-color: #90ee90"); //Sets color of button text to black and background to light green
             }
         }
     }
-}
+    
+} //End of LargeSelection class
