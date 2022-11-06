bfa8dd58d2990756888a5091cb568fa27ac6aaac
KineticKoi <64819201+KineticKoi@users.noreply.github.com>
2022-11-06 01:45:54 -0700

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,11 +1,11 @@
 
 //Employee class with getter and setter methods for employee role
 public class Employee extends User {
-    private String role; //role includes order processing agent and chef employees
+    private String role; //role can be either chef or order processing agent
     
     //Default Constructor
     Employee(String role) {
-        super("EMPLOYEE"); //Super to pull employee object information 
+        super("EMPLOYEE"); //Super for class User 
         this.role = role; //Updates employee object's role
     }
     
