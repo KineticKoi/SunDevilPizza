dc2f9f96b04d62fbff741b0f4e6492ffd7f01eb1
boon5 <kadinjrabo@gmail.com>
2022-11-10 14:40:06 -0700

    Comment readability

--- SunDevilPizza.java
+++ SunDevilPizza.java
@@ -9,34 +9,55 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
-public class SunDevilPizza extends Application { //Launches the main application
-    public static final String resourcesPath = "./resources/"; //defines resources directory
-    public static final String customerFilesPath = "./customerFiles/"; //defines area to save customer files
-    static final int width = 1920, height = 1080; //Initializes the width and height for the app window
-    private static List<Parent> rootNodes = new ArrayList<>(); //creates an arraylist for rootnodes
+//Launches the main application
+public class SunDevilPizza extends Application {
+    // defines resources directory
+    public static final String resourcesPath = "./resources/";
+    // defines area to save customer files
+    public static final String customerFilesPath = "./customerFiles/";
+    // Initializes the width and height for the app window
+    static final int width = 1920, height = 1080; 
+    // creates an arraylist for rootnodes
+    private static List<Parent> rootNodes = new ArrayList<>(); 
     private static int currentRootIndex = -1;
-    private static Scene scene; //initialize a new scene
-    public static Session session = new Session(); //create a new default sesssion
+    // initialize a new scene
+    private static Scene scene; 
+    // create a new default sesssion
+    public static Session session = new Session(); 
     
     @Override
     public void start(Stage stage) throws Exception {
-        scene = new Scene(new WelcomeUI(width, height), width, height); //Generates a new GUI scene
-        newRoot(scene.getRoot()); //sets a new root that is derived from the root of the new generated scene
-        stage.setTitle("SunDevil Pizza"); //Sets the window title
-        stage.setScene(scene); //Sets the window scene
+        // Generates a new GUI scene
+        scene = new Scene(new WelcomeUI(width, height), width, height); 
+        // sets a new root that is derived from the root of the new generated scene
+        newRoot(scene.getRoot()); 
+        // Sets the window title
+        stage.setTitle("SunDevil Pizza"); 
+        // Sets the window scene
+        stage.setScene(scene); 
         //try { //Custom cursor, icons, and CSS
         try {
-            Image icon = new Image(new FileInputStream(SunDevilPizza.resourcesPath + "sdpLogoIcon.png")); //pulls the sun devil pizza logo from the resources folder and sets the icon image as it
-            Image cursor = new Image(new FileInputStream(SunDevilPizza.resourcesPath + "cutter.png"));//pulls the pizza cutter image from the resource folder and sets the cursor as the pizza cutter
-            stage.getIcons().add(icon); //sets the icon on the stage
-            scene.setCursor(new ImageCursor(cursor)); //initalizes new cursor
-            scene.getStylesheets().add("sdp.css"); //uses the custom css to create a style for the scene
+            // pulls the sun devil pizza logo from the resources folder and sets the icon
+            // image as it
+            Image icon = new Image(new FileInputStream(SunDevilPizza.resourcesPath + "sdpLogoIcon.png")); 
+            // pulls the pizza cutter image from the resource folder and sets the cursor as
+            // the pizza cutter
+            Image cursor = new Image(new FileInputStream(SunDevilPizza.resourcesPath + "cutter.png"));
+            // sets the icon on the stage
+            stage.getIcons().add(icon); 
+            // initalizes new cursor
+            scene.setCursor(new ImageCursor(cursor)); 
+            // uses the custom css to create a style for the scene
+            scene.getStylesheets().add("sdp.css"); 
         }
         catch (Exception e) {
-            System.out.println(e); //if an exception is caught print in the console log
+            // if an exception is caught print in the console log
+            System.out.println(e); 
         }
-        stage.setFullScreen(true); //Sets app to fullscreen
-        stage.show(); //Shows the window
+        // Sets app to fullscreen
+        stage.setFullScreen(true); 
+        // Shows the window
+        stage.show(); 
     }
     
     //Main
@@ -53,31 +74,42 @@ public class SunDevilPizza extends Application { //Launches the main application
     
     //Sets Root to previous node
     public static void previousRoot() {
-        if (currentRootIndex == 1) { //if the root index is 1, go to node 0 which is home
+        // if the root index is 1, go to node 0 which is home
+        if (currentRootIndex == 1) { 
             home();
         }
         else {
             scene.setRoot(rootNodes.get(currentRootIndex - 1));
-            currentRootIndex--; //tracks what node of the page the user is on by moving one node back and updating the nodelist
+            // tracks what node of the page the user is on by moving one node back and
+            // updating the nodelist
+            currentRootIndex--; 
         }   
     }
     
     //Sets Root to next node
     public static boolean nextRoot() {
-        if (rootNodes.size() > currentRootIndex + 1) { //checks if the next node can exist
-            scene.setRoot(rootNodes.get(currentRootIndex + 1)); //sets the next root
-            currentRootIndex++; //updates the root index
-            return true; //returns true if the root was set to the next node
+        // checks if the next node can exist
+        if (rootNodes.size() > currentRootIndex + 1) { 
+            // sets the next root
+            scene.setRoot(rootNodes.get(currentRootIndex + 1)); 
+            // updates the root index
+            currentRootIndex++; 
+            // returns true if the root was set to the next node
+            return true; 
         }
-        return false; //returns false if the root is the last node
+        // returns false if the root is the last node
+        return false; 
     }
     
     //Returns app to welcome screen
     public static void home() {
-        for (int i = rootNodes.size() - 1; i > 0; i--) { //removes all of the nodes in the list iteratively
+        // removes all of the nodes in the list iteratively
+        for (int i = rootNodes.size() - 1; i > 0; i--) { 
             rootNodes.remove(i);
         }
-        currentRootIndex = 0; //resets the root node index to home
-        scene.setRoot(rootNodes.get(0)); //serts the new empty root to home
+        // resets the root node index to home
+        currentRootIndex = 0; 
+        // sets the new empty root to home
+        scene.setRoot(rootNodes.get(0)); 
     }
 }    
