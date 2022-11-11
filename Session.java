c35a5be5a9b9ca03ef3c8bf703da7c137ee3c2de
boon5 <kadinjrabo@gmail.com>
2022-11-10 20:01:11 -0700

    Cleanup

--- Session.java
+++ Session.java
@@ -1,32 +1,47 @@
-
 import java.util.Random;
 import java.util.UUID;
 
-public class Session {
-    private User user; //creates a user
-    private final UUID sessionID = UUID.randomUUID(); //creates a random session id
-    Session() {
+public class Session
+{
+    // creates a user
+    private User user;
+    // creates a random session id
+    private final UUID sessionID = UUID.randomUUID();
+    // sets the user as null by default
+    Session()
+    {
         user = null;
-    } //sets the user as null by default
+    }
 
-    public User getUser() {
+    // user getter
+    public User getUser()
+    {
         return user;
-    } //user getter
+    }
     
-    public void setUser(User user) {
+    // user setter
+    public void setUser(User user)
+    {
         this.user = user;
-    } //user setter
+    }
     
-    public UUID getSessionID() {
+    // session ID getter
+    public UUID getSessionID()
+    {
         return sessionID;
-    } //session ID getter
+    }
 
-    public String generateOrderNumber() { //generate a unique order number
+    // generate a unique order number
+    public String generateOrderNumber()
+    {
         Random rand = new Random();
-        int num = rand.nextInt(999999); //creating a new random number that will serve as the order number
-        String orderNumber = String.format("%06d", num); //converts the order number to a string filling the blanks with zeros up to 6 digits
-        orderNumber = orderNumber.substring(0, 2) + "-" + orderNumber.substring(2); //creates a delimiter between the zeros and the order number
-        return orderNumber; //returns the order number
+        // creating a new random number that will serve as the order number
+        int num = rand.nextInt(999999);
+        // converts the order number to a string filling the blanks with zeros up to 6 digits
+        String orderNumber = String.format("%06d", num);
+        // creates a delimiter between the zeros and the order number
+        orderNumber = orderNumber.substring(0, 2) + "-" + orderNumber.substring(2);
+        // returns the order number
+        return orderNumber;
     }
 }
-
