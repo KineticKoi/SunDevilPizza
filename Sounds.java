953b68e664e9b254e0d3516949321f6b01bd936f
boon5 <kadinjrabo@gmail.com>
2022-11-10 20:01:57 -0700

    Cleanup

--- Sounds.java
+++ Sounds.java
@@ -3,37 +3,49 @@ import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
-//Sounds class that manages all app audio...
-public class Sounds {
-    
-    //Takes a filepath parameter and plays that sound...
-    public static void playSound(String file) {
-        try {
-            File musicPath = new File(SunDevilPizza.resourcesPath + file); //Gets sound file
-            if (musicPath.exists()) { //If sound file exists...
+// Sounds class that manages all app audio...
+public class Sounds
+{    
+    // Takes a filepath parameter and plays that sound
+    public static void playSound(String file)
+    {
+        try
+        {
+            // Gets sound file
+            File musicPath = new File(SunDevilPizza.resourcesPath + file);
+            // If sound file exists
+            if (musicPath.exists())
+            {
                 AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
                 Clip clip = AudioSystem.getClip();
                 clip.open(audioInput);
                 clip.start();
             }
         }
-        catch(Exception e) {
+        catch(Exception e)
+        {
             e.printStackTrace();
         }
     }
     
-    //Plays button click
-    public static void playButtonClick() {
-        try {
-            File musicPath = new File(SunDevilPizza.resourcesPath + "click.wav"); //Gets sound file
-            if (musicPath.exists()) { //If sound file exists...
+    // Plays button click
+    public static void playButtonClick()
+    {
+        try
+        {
+            // Gets sound file
+            File musicPath = new File(SunDevilPizza.resourcesPath + "click.wav");
+            // If sound file exists...
+            if (musicPath.exists())
+            {
                 AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
                 Clip clip = AudioSystem.getClip();
                 clip.open(audioInput);
                 clip.start();
             }
         }
-        catch(Exception e) {
+        catch(Exception e)
+        {
             e.printStackTrace();
         }
     }
