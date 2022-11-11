18cd8c3a32cd65ad3e42b9db20aae515cb9293fc
boon5 <kadinjrabo@gmail.com>
2022-11-10 19:30:52 -0700

    Cleanup

--- FileManager.java
+++ FileManager.java
@@ -1,4 +1,3 @@
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -8,43 +7,76 @@ import java.io.NotSerializableException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
-//class to manage file assets
-public class FileManager {
-
-    public static void saveCustomer(Customer customer) { //saving the customer locally to the administrator's computer
-        try { //customer class contains order information too
-            FileOutputStream fileout = new FileOutputStream(SunDevilPizza.customerFilesPath + customer.getIDNum() + ".dat"); //creating a new data file to hold output for the specific customer
-            ObjectOutputStream streamOut = new ObjectOutputStream(fileout); //creating a new object data stream
-            streamOut.writeObject(customer); //writing customer data to the output
+// class to manage file assets
+public class FileManager
+{
+    // saving the customer locally to the administrator's computer
+    public static void saveCustomer(Customer customer) {
+        // customer class contains order information too
+        try
+        {
+            // creating a new data file to hold output for the specific customer
+            FileOutputStream fileout = new FileOutputStream(SunDevilPizza.customerFilesPath + customer.getIDNum() + ".dat");
+            // creating a new object data stream
+            ObjectOutputStream streamOut = new ObjectOutputStream(fileout);
+            // writing customer data to the output
+            streamOut.writeObject(customer);
             streamOut.close();
-            fileout.close(); //closing stream
-        } catch(NotSerializableException n) { //printing exceptions to console log
+            // closing stream
+            fileout.close();
+        }
+        // printing exceptions to console log
+        catch(NotSerializableException n)
+        {
             System.out.println("Not serializable exception\n");
-        } catch(IOException e) {
+        }
+        catch(IOException e)
+        {
             System.out.println("Data file read exception\n");
         }
     }
     
-    public static Customer loadCustomer(String asuriteID) { //loading customers from the data file
-        try {
-            FileInputStream fileIn = new FileInputStream(SunDevilPizza.customerFilesPath + asuriteID + ".dat"); //reading data file for the customer if it exists
-            ObjectInputStream in = new ObjectInputStream(fileIn); //creating a data stream
-            Customer customer = (Customer)in.readObject();//reading the file to see if the customer is present
+    // loading customers from the data file
+    public static Customer loadCustomer(String asuriteID)
+    {
+        try
+        {
+            // reading data file for the customer if it exists
+            FileInputStream fileIn = new FileInputStream(SunDevilPizza.customerFilesPath + asuriteID + ".dat");
+            // creating a data stream
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            // reading the file to see if the customer is present
+            Customer customer = (Customer)in.readObject();
             return customer;
-        } catch(ClassNotFoundException c) { //if the customer class is not found
+        }
+        // if the customer class is not found
+        catch(ClassNotFoundException c)
+        {
            System.out.println("Class not found\n");    
-        } catch (FileNotFoundException f) { //if the specific customer does not exist
+        }
+        // if the specific customer does not exist
+        catch (FileNotFoundException f)
+        {
             System.out.println("No customer save exists\n");
-        } catch(NotSerializableException n) { //not serializable or data exception
+        }
+        // not serializable or data exception
+        catch(NotSerializableException n)
+        {
             System.out.println("Not serializable exception\n");
-        } catch(IOException e) {
+        }
+        catch(IOException e)
+        {
            System.out.println("Data file read exception\n");
         }
         return null;
     }
     
-    public static boolean existingCustomer(String asuriteID) { //if the customer is already existing
-        File f = new File(SunDevilPizza.customerFilesPath + asuriteID + ".dat"); //creating a new file for the customer
-        return f.exists(); //return file exists
+    // if the customer is already existing
+    public static boolean existingCustomer(String asuriteID)
+    {
+        // creating a new file for the customer
+        File f = new File(SunDevilPizza.customerFilesPath + asuriteID + ".dat");
+        // return file exists
+        return f.exists();
     }
 }
