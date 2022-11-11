ed991bde97c002d11732192b11af99f9ef8513dc
boon5 <kadinjrabo@gmail.com>
2022-11-10 19:17:34 -0700

    Cleanup

--- Employee.java
+++ Employee.java
@@ -1,17 +1,21 @@
-
-//Class with getter and setter methods for employee role
-public class Employee extends User {
-    private String role; //Role can be either Chef or Order Processing Agent
+// Class with getter and setter methods for employee role
+public class Employee extends User
+{
+    // Role can be either Chef or Order Processing Agent
+    private String role;
     
-    //Default Constructor
-    Employee(String role) {
+    // Default Constructor
+    Employee(String role)
+    {
         super("EMPLOYEE");
-        this.role = role; //Updating employee role from string in paratemeter list
+        // Updating employee role from string in paratemeter list
+        this.role = role;
     }
     
-    //Getter method for returning employee role
-    public String getRole() {
+    // Getter method for returning employee role
+    public String getRole()
+    {
         return role;
     }
     
-} //End of Employee class
+}
