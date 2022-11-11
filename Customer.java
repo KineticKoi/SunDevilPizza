8d00dd4b7368dd2b13975f9d67b5e37e5504a2d7
boon5 <kadinjrabo@gmail.com>
2022-11-10 19:10:06 -0700

    Cleanup

--- Customer.java
+++ Customer.java
@@ -1,68 +1,80 @@
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
 
-//Customer class containing orders and customer info...
-public class Customer extends User implements Serializable {
+// Customer class containing orders and customer info...
+public class Customer extends User implements Serializable
+{
     private static final long serialVersionUID = 42069L;
     
-    //Declaring Variables...
+    // Declaring Variables
     private String asuriteIDNum;
     private List<Order> orderHistory;
     private Order currentOrder;
     private String password;
     
-    //Default Constructor
+    // Default Constructor
     Customer() {}
     
-    //Building Customer object from relevant information
-    Customer(String asuriteIDNum) {
+    // Building Customer object from relevant information
+    Customer(String asuriteIDNum)
+    {
         super("CUSTOMER");
-        this.asuriteIDNum = asuriteIDNum; //Sets new object's customer ASURITEID number
-        this.currentOrder = new Order(new Pizza()); //Sets current order using new customized pizza
-        this.orderHistory = new ArrayList<>(); //Creates new arrayList for customer object's order history
+        // Sets new object's customer ASURITEID number
+        this.asuriteIDNum = asuriteIDNum;
+        // Sets current order using new customized pizza
+        this.currentOrder = new Order(new Pizza());
+        // Creates new arrayList for customer object's order history
+        this.orderHistory = new ArrayList<>();
     }
     
-    //Getter method for returning ASURITEID number
-    public String getIDNum() {
+    // Getter method for returning ASURITEID number
+    public String getIDNum()
+    {
         return asuriteIDNum;
     }
     
-    //Setter method for setting ASURITEID number
-    public void setIDNum(String asuriteIDNum) {
+    // Setter method for setting ASURITEID number
+    public void setIDNum(String asuriteIDNum)
+    {
         this.asuriteIDNum = asuriteIDNum;
     }
     
-    //Setter method for adding current order to customer's order history list
-    public void addOrder(Order order) {
+    // Setter method for adding current order to customer's order history list
+    public void addOrder(Order order)
+    {
         orderHistory.add(order);
     }
     
-    //Gettter method for returning customer's current order
-    public Order getCurrentOrder() {
+    // Gettter method for returning customer's current order
+    public Order getCurrentOrder()
+    {
         return currentOrder;
     }
     
-    //Setter method for setting current order's pizza type, toppings, and size from customization
-    public void resetCurrentOrder() {
+    // Setter method for setting current order's pizza type, toppings, and size from customization
+    public void resetCurrentOrder()
+    {
         currentOrder = new Order(new Pizza());
     }
     
-    //Getter method for returning password
-    public String getPassword() {
+    // Getter method for returning password
+    public String getPassword()
+    {
         return password;
     }
     
-    //Setter method for setting password
-    public void setPassword(String password) {
+    // Setter method for setting password
+    public void setPassword(String password)
+    {
         this.password = password;
     }
     
-    //Getter method for returning customer order history list
-    public List<Order> getOrderHistory() {
+    // Getter method for returning customer order history list
+    public List<Order> getOrderHistory()
+    {
         return orderHistory;
     }
     
-} //End of customer class
+}
