0cc71b0056d1560bc8b308a2e4b929600a6711e9
boon5 <kadinjrabo@gmail.com>
2022-11-10 20:04:42 -0700

    Cleanup

--- WelcomeUI.java
+++ WelcomeUI.java
@@ -10,16 +10,18 @@ import javafx.scene.layout.Pane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-public class WelcomeUI extends Pane {
-    //Declaring Variables...
+public class WelcomeUI extends Pane
+{
+    // Declaring Variables
     private ImageView mainLogoBanner;
     private Button startButton;
     private Button loginButton;
     private int clickCount = 0;
     private Label counter;
     
-    //Constructor
-    WelcomeUI(int width, int height) {
+    // Constructor
+    WelcomeUI(int width, int height)
+    {
         // Sets this pane width
         setWidth(width); 
         // Sets this pane height
@@ -28,7 +30,8 @@ public class WelcomeUI extends Pane {
         counter = new Label("");
         counter.setFont(new Font("Arial", 24));
         counter.relocate(180, 920);
-        try {
+        try
+        {
             // creates an image object to be used
             Image logoImage = new Image(new FileInputStream("./resources/sdpLogo.png"));
             // creates a image view object to interact with the pane spaces
@@ -41,41 +44,51 @@ public class WelcomeUI extends Pane {
             mainLogoBanner.layoutXProperty().bind(this.widthProperty().subtract(mainLogoBanner.getFitWidth()).divide(2));
             mainLogoBanner.layoutYProperty().set(140);
             // Balasooriya mode easter egg
-            mainLogoBanner.setOnMouseClicked(event ->  {
-                if (clickCount < 420) {
+            mainLogoBanner.setOnMouseClicked(event ->
+            {
+                if (clickCount < 420)
+                {
                     clickCount++;
                 }
-                if (clickCount >= 69) {
-                    try {
+                if (clickCount >= 69)
+                {
+                    try
+                    {
                         // pulls the pizza cutter image from the resource folder and sets the cursor as
                         // the pizza cutter
                         Image cursor = new Image(new FileInputStream(SunDevilPizza.resourcesPath + "bala.png"));
                         // initalizes new cursor
                         this.getScene().setCursor(new ImageCursor(cursor));
                     }
-                    catch (Exception e) {
+                    catch (Exception e)
+                    {
                         // if an exception is caught print in the console log
                         System.out.println(e);
                     }
                 }
-                if (clickCount == 420) {
-                    try {
+                if (clickCount == 420)
+                {
+                    try
+                    {
                         // pulls the pizza cutter image from the resource folder and sets the cursor as
                         // the pizza cutter
                         Image cursor = new Image(new FileInputStream(SunDevilPizza.resourcesPath + "cutter.png"));
                         // initalizes new cursor
                         this.getScene().setCursor(new ImageCursor(cursor));
                     }
-                    catch (Exception e) {
+                    catch (Exception e)
+                    {
                         // if an exception is caught print in the console log
                         System.out.println(e);
                     }
-                    try {
+                    try
+                    {
                         // sets the icon on the stage  
                         Image icon = new Image(new FileInputStream(SunDevilPizza.resourcesPath + "bala.png"));
                         ((Stage)this.getScene().getWindow()).getIcons().add(icon);
                     }
-                    catch (Exception e) {
+                    catch (Exception e)
+                    {
                         // if an exception is caught print in the console log
                         System.out.println(e);
                     }
@@ -83,7 +96,9 @@ public class WelcomeUI extends Pane {
                 counter.setText(String.valueOf(clickCount));
             });
         }
-        catch(Exception e) {     
+        catch(Exception e)
+        {
+
         }
         // Creates a login button
         loginButton = new ButtonMaker("login");
@@ -102,22 +117,27 @@ public class WelcomeUI extends Pane {
     }
     
     // control handler for the welcome page
-    private class WelcomePageControlsHandler implements EventHandler<ActionEvent> {
+    private class WelcomePageControlsHandler implements EventHandler<ActionEvent> 
+    {
         @Override
-        public void handle(ActionEvent event) {
+        public void handle(ActionEvent event) 
+        {
             // when button is clicked play a sound
             Sounds.playButtonClick();
             // if login button is clicked
-            if (event.getSource() == loginButton) {
+            if (event.getSource() == loginButton) 
+            {
                 // new LoginUI
                 SunDevilPizza.newRoot(new LoginUI("ASURITE", SunDevilPizza.width, SunDevilPizza.height));
             }
             // if login button is clicked
-            else if (event.getSource() == startButton) {
+            else if (event.getSource() == startButton) 
+            {
                 // new pizza builder UI
                 SunDevilPizza.newRoot(new PizzaBuilderUI(SunDevilPizza.width, SunDevilPizza.height)); 
                 // if there is no user
-                if (SunDevilPizza.session.getUser() == null) { 
+                if (SunDevilPizza.session.getUser() == null)
+                { 
                     // create temp user
                     SunDevilPizza.session.setUser(new Customer("-1")); 
                     // setting type of the session
